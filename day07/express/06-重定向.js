52f216a9c7707a97a10547aedf22f3eeb893e7e8
qianbuer <1085445531@qq.com>
2022-01-11 17:36:08 +0800

    nodejs学习

---
+++ day07/express/06-重定向.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = new express();
+// 1.使用express官方中间件
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); // 解析json格式
+
+app.get('/register', (req, res) => {
+	let filePath = path.join(__dirname, 'views', 'register.html');
+
+	// 读取文件内容
+	let content = fs.readFileSync(filePath, 'utf-8');
+
+	// 访问/register时返回注册页面
+	res.send(content);
+});
+
+app.post('/register', (req, res) => {
+	// 一般post提交过来，需要获取到请求参数
+
+	// 2.获取参数 通过req.body获取参数是一个对象
+	console.log(req.body);
+
+	// 结构一下，并打印
+	let { username, email, password, repwd } = req.body;
+	console.log(username);
+	console.log(email);
+	console.log(password);
+	console.log(repwd);
+
+	// 准备重定向
+	res.send('post');
+});
+
+app.get('/login', (req, res) => {
+	let filePath = path.join(__dirname, 'views', 'login.html');
+
+	// 读取文件内容
+	let content = fs.readFileSync(filePath, 'utf-8');
+
+	// 访问/register时返回注册页面
+	res.send(content);
+});
+
+app.listen(3000, err => {
+	if (err) {
+		console.log(err);
+		return;
+	}
+	console.log('express web server is listen in port 3000! ');
+});
