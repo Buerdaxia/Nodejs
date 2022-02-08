cb940497f054572b98864ff906b0e6a39e5c82fb
钱不二 <1085445531@qq.com>
2022-02-08 23:18:38 +0800

    node连接数据库

---
+++ day09/Express/01-使用mysql模块获取数据库数据.js
@@ -0,0 +1,23 @@
+const express = require('express');
+// 1.引入db.js文件中的db
+const db = require('./db/db.js');
+
+const app = new express();
+
+app.get('/get_data', (req, res) => {
+	// 2.使用db暴露出来的query函数
+	/*
+    err: 报错信息
+    data: 查询出来的数据
+  */
+	db.query('select * from students where gender = 1', (err, data) => {
+		console.log(data);
+		// 3.对data进行处理
+		res.send(data);
+	});
+	// res.send('hello word');
+});
+
+app.listen(4000, () => {
+	console.log('服务器以及启动, 端口为: 4000');
+});
---
+++ day09/Express/db/db.js
@@ -0,0 +1,23 @@
+var mysql = require('mysql');
+
+var pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: '123456',
+	database: 'qianduan_test'
+}); // 连接数据库的配置
+
+/*
+  sql: sql语句
+  callback: 回调函数
+*/
+function query(sql, callback) {
+	pool.getConnection(function(err, connection) {
+		connection.query(sql, function(err, rows) {
+			callback(err, rows);
+			connection.release();
+		});
+	});
+}
+
+exports.query = query;
